ba244129405c965aa206d76d5fdc19b150e30fc2
bingzhe <874177815@qq.com>
2022-06-08 14:00:03 +0800

    config

--- common/config.js
+++ common/config.js
@@ -7,7 +7,8 @@ let config = {
 if (process.env.NODE_ENV == 'development') { // 开发环境
 	config = {
 		imgUrl: 'https://tuan.100dtc.com',
-		baseUrl: 'https://tuan.100dtc.com/',
+		// baseUrl: 'https://tuan.100dtc.com/',
+		baseUrl: 'http://retail.shouchuangtx.cn/' // 一亩莲
 		// websocketUrl: "wss://spike.bdtc100.com:2345",
 	}
 }
